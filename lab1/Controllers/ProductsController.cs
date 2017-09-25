024f7bde6e28f14184ab5f9add0860212ca8e441
marble2388 <jefftheperson1@hotmail.com>
2017-09-25 18:26:04 -0400

    Finished Lab1 Final Step
    Upload and attempt to make azure do something with it

---
+++ lab1/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using lab1.Models;
+
+namespace lab1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            //create typed list of product using detials model
+            var details = new List<Detail>();
+            details.Add(new Detail { Name = "Laptop" });
+            details.Add(new Detail { Name = "Tablet" });
+            details.Add(new Detail { Name = "Desktop" });
+            return View(details);
+        }
+        //GET: Product Details
+        public ActionResult Details(string details)
+        {
+            ViewBag.Details = details;
+            return View();
+        }
+
+
+    }
+}
---
+++ lab1/Models/Detail.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lab1.Models
+{
+    public class Detail
+    {
+        public string Name { get; set; }
+    }
+}
